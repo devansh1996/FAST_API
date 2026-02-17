f31ff83df56ae912255b078c6502acb951c90f3c
DevanshSharma <devansh.pec@gmail.com>
2026-02-17 20:26:04 +0100

    PUT updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 from fastapi import FastAPI,Path,Query,HTTPException
 from fastapi.responses import JSONResponse
-from typing import Annotated,Literal
+from typing import Annotated,Literal,Optional
 import pydantic
 from pydantic import BaseModel,Field,computed_field
 app=FastAPI()
@@ -26,7 +26,7 @@ class Patient(BaseModel):
             return 'underweight'
         elif self.bmi<25:
             return 'normal'
-        elif self.bmi>25:
+        elif self.bmi>=25:
             return 'overweight'
         
 def load_data():
@@ -62,6 +62,39 @@ def create_patient(patient:Patient):
     ## save into json
     save_data(data) 
     return JSONResponse(status_code=201,content={'message':'patient create successfully'})
+
+class PatientUpdate(BaseModel):
+    id:str
+    age:Annotated[Optional[int],Field(default=None)]
+    name:Annotated[Optional[str],Field(default=None)]
+    city:Annotated[Optional[str],Field(default=None)]
+    gender:Annotated[Optional[Literal['Male','Female','Others']],Field(default=None)]
+    height:Annotated[Optional[float],Field(default=None)]
+    weight:Annotated[Optional[float],Field(default=None)]
+
+
+
+@app.put('/edit/{patient_id}')
+def update_date(patient_id:str,patient_update:PatientUpdate):
+    data=load_data()
+    if patient_id not in data:
+        raise HTTPException(status_code=404,detail='patient not found')
+    existing_patient_info=data[patient_id]
+    updated_patient_info=patient_update.model_dump(exclude_none=True) # important to include FIELDS MEANT TO UPDATE NOT ALL
+
+    for key,value in updated_patient_info.items():
+        existing_patient_info[key]=value
+    ## convert exisint_patient_info to patient object
+    ## so bmi is recalculated with the new value
+    existing_patient_info['id']=patient_id
+    patient_update_pydantic_obj=Patient(**existing_patient_info)
+    ## converting this back to dictionary because bmi is calculated
+    existing_patient_info= patient_update_pydantic_obj.model_dump(exclude='id')
+    data[patient_id]=existing_patient_info
+    #save data
+    save_data(data)
+    return JSONResponse(status_code=200,content='updated')
+
 
 def save_data(data):
     with open('patients.json','w') as f:
